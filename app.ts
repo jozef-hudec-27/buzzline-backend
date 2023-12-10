ba9f22b3eea0643db09ce7f7a561a3c65161b745
Jozef Hudec <jojohudec27@gmail.com>
2023-12-10 10:30:06 +0100

    initial commit

---
+++ app.ts
@@ -0,0 +1,38 @@
+import express, { Express, Request, Response, NextFunction, Errback } from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import createError from 'http-errors'
+import logger from 'morgan'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// routers
+import apiRouter from './routers/apiRouter'
+
+const app: Express = express()
+
+// middleware
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', apiRouter)
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err: any, req: Request, res: Response) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  res.status(err.status || 500).json({ message: err.message || 'There was an error.' })
+})
+
+export default app
