b83b905f0fc322e40d144bc8a281905302b2a766
Jozef Hudec <jojohudec27@gmail.com>
2023-12-10 11:52:15 +0100

    Add chatToken to User

--- models/user.ts
+++ models/user.ts
@@ -1,11 +1,26 @@
 import mongoose from 'mongoose'
+import crypto from 'crypto'
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, maxlength: 100 },
   firstName: { type: String, required: true, maxlength: 100 },
   lastName: { type: String, required: true, maxlength: 100 },
   passwordEncrypted: { type: String, required: true },
-  tokens: { type: [String], default: [] },
+  refreshTokens: { type: [String], default: [] },
+  chatToken: { String, default: generateChatToken, unique: true }, // Used to create chat with user
 })
 
-export default mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema)
+
+async function generateChatToken(): Promise<string> {
+  const token = crypto.randomBytes(32).toString('hex')
+  const user = await User.findOne({ chatToken: token })
+
+  if (user) {
+    return await generateChatToken()
+  }
+
+  return token
+}
+
+export default User
